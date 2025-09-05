bf8e600a4f0433d7d8fdd8c137e92cf7a202f4f2
Lenka Karthikeya <162859164+KARTHIKEYA-LENKA@users.noreply.github.com>
2025-09-05 21:55:05 +0530

    Create DISPLAYCURRENTSTATE.js

---
+++ LESSON1/DISPLAYCURRENTSTATE.js
@@ -0,0 +1,7 @@
+function onLoad()
+{
+	alert('Current state value is: '+g_form.getValue('state'));
+}
+//choose table - incident or another 
+//choose type-onLoad (By choosing onload when page is reloded the alert will be populated )
+//write the above script in the script block of service now
